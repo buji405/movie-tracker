8f2c9c9cea400b7f90f7389ebe9e02f565634ac6
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-12 13:01:32 -0600

    Add routerReducer to rootReducer.

--- app/index.js
+++ app/index.js
@@ -4,24 +4,13 @@ import App from './components/app';
 import MovieIndex from './components/movieIndex';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-
 import createHistory from 'history/createBrowserHistory';
-
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
-
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import rootReducer from './reducers';
-
 import ReduxThunk from 'redux-thunk';
-
 const history = createHistory();
-
 const middleware = routerMiddleware(history);
-
 const store = configureStore()
-
-// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-// TODO: figure out how to get router: router
 
 function configureStore(initialState) {
   return createStore(
@@ -33,13 +22,6 @@ function configureStore(initialState) {
     )
   );
 }
-// const store = createStore(combineReducers({
-//   router: routerReducer
-// }),
-//   devTools,
-//   applyMiddleware(middleware),
-//   applyMiddleware(thunk),
-// )
 
 ReactDOM.render(
 <Provider store={store}>
