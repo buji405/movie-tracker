7f5b61dfec55c224c55c2ba2503c56dbd0226163
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-14 00:02:17 -0600

    Add refactor to containers.

--- app/containers/loginContainer.js
+++ app/containers/loginContainer.js
@@ -2,16 +2,16 @@ import { connect } from 'react-redux';
 import Login from '../components/Login/Login';
 import { usersFetchData, addUser } from '../actions';
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchUserData: (url, email, password) => dispatch(usersFetchData(url, email, password)),
-    fetchNewUser: (url, email, password, name) => dispatch(addUser(url, email, password, name)),
-  }
-}
-
 const mapStateToProps = (state) => {
   return {
     state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUserData: (url, email, password) => dispatch(usersFetchData(url, email, password)),
+    fetchNewUser: (url, email, password, name) => dispatch(addUser(url, email, password, name))
   }
 }
 
