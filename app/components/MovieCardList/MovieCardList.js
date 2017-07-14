7d5a9ea57c6ca00faf352847abc72305100dee83
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-13 23:56:38 -0600

    Remove redundant API calls from componentDidMount and refactor in MovieCardList.

--- app/components/MovieCardList/MovieCardList.js
+++ app/components/MovieCardList/MovieCardList.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react'
-import MovieCard from '../MovieCard/MovieCard'
+import React, { Component } from 'react';
+import MovieCard from '../MovieCard/MovieCard';
 import { itemsFetchData } from '../../actions';
-import { key } from '../../key.js'
+import { key } from '../../key.js';
 
 class MovieCardList extends Component {
 
   componentDidMount() {
-    this.props.fetchData(`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`)
-
+    if (this.props.items.length === 0) {
+      this.props.fetchData(`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`)
+    }
   }
 
   render() {
     let cards;
 
     if (this.props.hasErrored) {
-      return <p>Sorry Asshole</p>
+      return <p>Please refresh Problem loading page</p>
     }
-
-    //if this.props.invalidLogin, render you have entered the wrong credentials message.
 
     if (this.props.isLoading) {
       return <p>Loading...</p>
@@ -28,15 +27,16 @@ class MovieCardList extends Component {
         return (
           <MovieCard poster={result.poster_path}
                      key={index}/>
-        )
-      })
+          );
+      });
     }
+
     return (
       <section className="movies-container">
         {cards}
       </section>
-    )
+    );
   }
 }
 
-export default MovieCardList
+export default MovieCardList;
